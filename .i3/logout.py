bece296ba50c65eef3b7fea58494f47e314e17ac
alex <alex@thinkpad.lan>
2015-01-22 19:38:43 +0100

    ADDED: window_event handler for logout

--- .i3/logout.py
+++ .i3/logout.py
@@ -37,14 +37,19 @@ class EventHandler (object):
     def lock_clicked(self, button):
       print("lock")
       self.lock()
-    def key_pressed(self, widget, event):
-      #print("key: ", event.keyval, "; state: ", event.state)
-      if event.keyval == 65307:
-        Gtk.main_quit()
-      return False 
     def lock(self):
       subprocess.Popen(['/usr/bin/slock'])
       Gtk.main_quit()
+    def key_pressed(self, widget, event):
+      #print("key: ", event.keyval, "; state: ", int(event.state))
+      if event.keyval == 65307:
+        Gtk.main_quit()
+      return False
+    def window_state_changed(self, widget, event):
+      if (int(event.new_window_state) & Gdk.WindowState.FULLSCREEN) == 0:
+        widget.fullscreen()
+      elif (int(event.new_window_state) & Gdk.WindowState.ICONIFIED) != 0:
+        widget.deiconify()
 
 class MyWin (Gtk.Window):
     _handler = EventHandler()
@@ -69,6 +74,7 @@ class MyWin (Gtk.Window):
       self.connect("draw", self.area_draw)
       self.connect("delete-event", Gtk.main_quit)
       self.connect("key-press-event", self._handler.key_pressed)
+      self.connect("window-state-event", self._handler.window_state_changed)
       builder.connect_signals(handlers)
 
       eb = builder.get_object("eventbox")
@@ -76,10 +82,7 @@ class MyWin (Gtk.Window):
 
       lh = builder.get_object("label_head")
       lh.set_text("Logout: " + getpass.getuser())
-      self.screen = self.get_screen()
-      self.visual = self.screen.get_rgba_visual()
-      if self.visual != None and self.screen.is_composited():
-         self.set_visual(self.visual)
+
       self.fullscreen()
       self.show_all()
 
