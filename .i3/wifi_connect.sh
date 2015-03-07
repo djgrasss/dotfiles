eeef31227c32c58acda3d4813e8bbece3f0441cc
alex <alex@chrome>
2015-03-07 16:33:00 +0100

    ADDED: keyb. shortcut for the wifi_connect

--- .i3/wifi_connect.sh
+++ .i3/wifi_connect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #ap=$(nmcli dev wifi | tail -n +2| awk -F"'" '{print $2}' | dmenu -i)
-aps=$(nmcli dev wifi | tail -n +2| awk -F"'" '{print $2}')
+aps=$(/usr/bin/nmcli dev wifi | tail -n +2| /usr/bin/awk -F"'" '{print $2}')
 i=1
 IFS=$'\n'
 for n in $aps; do
@@ -11,14 +11,14 @@ done
 ap=$(/usr/bin/zenity --entry --title "Available AP" --text "Choose AP to join" --entry-text "${apv[@]}")
 [ -z "$ap" ] && exit 1
 
-found=$(nmcli con list | awk -v pattern="$ap" '$0 ~ pattern {print "found"}')
+found=$(/usr/bin/nmcli con list | /usr/bin/awk -v pattern="$ap" '$0 ~ pattern {print "found"}')
 if [ -n "$found" ]; then
- nmcli con up id "$ap"
+ /usr/bin/nmcli con up id "$ap"
 else
   pass=$(/usr/bin/zenity --password --title "AP password:" --text "Enter AP password:")
   [ -n "$pass" ] && {
 #gksu -- nmcli dev wifi connect "$ap" password "$pass" --private
-    nmcli dev wifi connect "$ap" password "$pass" --private
+    /usr/bin/nmcli dev wifi connect "$ap" password "$pass" --private
   }
 fi
 
