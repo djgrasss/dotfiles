0d194b86029618b19099b73815d1c2527cdd1ca3
Alex <flux242@gmail.com>
2015-06-29 08:32:33 +0200

    CHANGED: slideshow refactored

--- .bin/slideshow.sh
+++ .bin/slideshow.sh
@@ -5,8 +5,8 @@ print_usage_exit()
   {
   [ -n "$1" ] && echo -e "ERROR: $1\n"
   echo "Usage: $0 [-dN] [-pN] directory_path"
-  echo "       -d N sets slideshow delay in seconds; default is 3"
-  echo "       -p N port to bind; default 12345"
+  echo "       -d N sets slideshow delay in seconds (default is 3)"
+  echo "       -p N port to bind (default is 12345). If port is 0 then binds to stdout"
   } >&2
   exit 1
 }
@@ -27,14 +27,18 @@ shift $((OPTIND-1))
 [ $DELAY -eq $DELAY ] 2>/dev/null || print_usage_exit "Delay argument is $DELAY but it should be an integer"
 [ $PORT -eq $PORT ] 2>/dev/null || print_usage_exit "Port argument is $PORT but it should be an integer"
 [ -d "$1" ] || print_usage_exit "Directory $1 does not exist"
+if [ $PORT -eq 0 ];then PIPECMD=cat;else PIPECMD="nc -l $PORT";fi
 
-(while :; do for i in $1/*;do echo "$i"; sleep $DELAY;done;done) | \
-  (echo -ne "HTTP/1.1 200 OK\r\nContent-Type: multipart/x-mixed-replace;";\
-   echo -ne "boundary=myboundary\r\n\r\n--myboundary\r\n";\
-   while read pic; do \
-     echo -ne "Content-Type: image/jpeg\r\n\r\n";\
-     cat "$pic";\
-     echo -ne "\r\n--myboundary\r\n";\
-   done \
-  ) | nc -l $PORT
+(
+   echo -ne "HTTP/1.1 200 OK\r\nContent-Type: multipart/x-mixed-replace;"
+   echo -ne "boundary=myboundary\r\n\r\n--myboundary\r\n"
+   while true; do
+   for i in $1/*; do
+     echo -ne "Content-Type: image/jpeg\r\n\r\n"
+     cat "$i"
+     echo -ne "\r\n--myboundary\r\n"
+     sleep $DELAY
+   done
+   done
+) | $PIPECMD
 
