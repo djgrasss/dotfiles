f37ec2e36c50355925eb21f4ca5c83c3ed92dea8
Alex <flux242@gmail.com>
2015-08-15 19:21:31 +0200

    CHANGED: cosmetic fixes to the conky bar

--- .bin/apstatus.sh
+++ .bin/apstatus.sh
@@ -10,25 +10,12 @@ r=$(iwconfig $iface|awk '/ESSID:/{split($0,a,/\"/);print a[2]};\
 IFS=$'\n'
 res=($r)
 
-# to avoid status line jumping quality is multiplied with 99 below
-
-if [[ "$1" == "-n" ]]; then
-{
-  printf "${res[0]}\n"
-  exit 0
-}
-elif [[ "$1" == "-f" ]]; then
-{
-  echo "${res[1]} GHz"
-  exit 0
-}
-elif [[ "$1" == "-s" ]]; then
-{
-  echo "$((99*${res[2]}))"
-  exit 0
-}
-fi
-
-printf "${res[0]} [$((99*${res[2]}))] ${res[1]} GHz\n"
-
+# Link quality is shown in percent but to avoid status 
+# line jumping it's multiplied by 99 and not by 100
+case $2 in 
+  -n) printf "${res[0]}\n" ;;
+  -f) echo "${res[1]} GHz" ;;
+  -s) echo "$((99*${res[2]}))" ;;
+   *) printf "${res[0]} [$((99*${res[2]}))] ${res[1]} GHz\n" ;;
+esac
 
