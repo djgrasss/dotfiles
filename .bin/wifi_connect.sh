01ed465f22302b339d2ad5c1cce5aab70709c796
alex <flux242@gmail.com>
2015-08-28 12:11:29 +0200

    CHANGED: wifi_connect shows signal strength now and allows to disconnect an iface

--- .bin/wifi_connect.sh
+++ .bin/wifi_connect.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-aps=$(nmcli dev wifi | tail -n +2| /usr/bin/awk -F"'" '{printf("%s (%s)\n", $2, gensub(/^\ *([^\ ]*).*/,"\\1","g",$3))}'|sort|uniq)
+aps=$(nmcli dev wifi | tail -n +2|/usr/bin/awk -F"'" '{printf("%s (%s) %s\n", $2, gensub(/^\ *([^\ ]*).*/,"\\1","g",$3), gensub(/[^\/]*[^0-9]*([0-9]*).*/,"\\1","g",$3))}'|sort|uniq)
 i=1
 IFS=$'\n'
 #IFS=$';'
+ifacecon=$(nmcli dev status | awk '$3 ~ "connected" {printf("disconnect (%s)\n",$1)}')
+for n in $ifacecon; do
+  apv[i]=$n
+  i=$((i+1))
+done
 for n in $aps; do
   apv[i]=$n
   i=$((i+1))
@@ -12,6 +17,15 @@ ap=$(/usr/bin/zenity --entry --title "Available AP" --text "Choose AP to join" -
 [ -z "$ap" ] && exit 1
 ap_name=$(echo "$ap"|sed -nr 's/(.*) \(.*/\1/p')
 ap_bssid=$(echo "$ap"|sed -nr 's/[^(]*\(([^)]*).*/\1/p')
+[ "$ap_name" = "disconnect" ] && {
+  # disconnect iface
+  for n in $ifacecon; do
+    [ "$n" = "$ap" ] && {
+      nmcli dev disconnect iface "${ap_bssid#* }"
+      exit 0
+    }
+  done
+}
 found=$(nmcli con list id "$ap_name" 2>/dev/null|awk -v bssid="$ap_bssid" '$0 ~ /seen-bssids/{if (1==index($2,bssid)){print "found"}}')
 if [ -n "$found" ]; then
   /usr/bin/nmcli con up id "$ap_name"
