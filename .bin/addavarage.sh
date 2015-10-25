e2bfdac42c1ead3d5da1c0c0e732a22bdbe57aea
Alex <flux242@gmail.com>
2015-10-25 20:44:52 +0100

    ADDED: addavarage.sh works now with multiple column input

--- .bin/addavarage.sh
+++ .bin/addavarage.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 avs=${1:-10}     # avarage over so many last samples, default is 10 
+column=${2:-1}   # read new value from this column
 sum=0
 
 while read newLine; do
-  [ -n "$newLine" ] && {
+  newVal="$(echo $newLine|awk -v col=$column '{print $col}')"
+  [ -n "$newVal" ] && {
     [ "${#a[@]}" -ge $avs ] && {
       sum=$((sum - ${a[0]}))
       a=(${a[@]:1}) # pop from the front
     }
-    a=("${a[@]}" "$newLine") # add to the end
-    sum=$((sum + newLine))
+    a=("${a[@]}" "$newVal") # add to the end
+    sum=$((sum + newVal))
     if [ -t 1 ]; then
       echo -ne "\033[s$newLine $((sum/avs))\033[K\033[u"
     else
