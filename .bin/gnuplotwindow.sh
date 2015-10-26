8d712c3b4bba4ec834fc71f25ccc8d36444e3038
alex <flux242@gmail.com>
2015-10-26 18:26:02 +0100

    CHANGED: gnuplotwindow.sh is reworked

--- .bin/gnuplotwindow.sh
+++ .bin/gnuplotwindow.sh
@@ -2,16 +2,13 @@
 
 winsize=${1:-60} # number of samples to store
 maxval=${2:-100} # maximum value of displayed range
-legend1=${3:-Current} #
-
-# this function is just for debugging gnuplot input
-gnuplott () {
-  echo "$2"
-}
 
 terminal="wxt"   # terminal type (x11,wxt,qt)
+
 samples=0        # samples counter
 IFS='\n'
+colors=( "red" "blue" "green" "yellow" )
+titles=( "$3" "$4" "$5" "$6" )
 while read newLine; do
   [ -n "$newLine" ] && {
     nf=$(echo "$newLine"|awk '{print NF}')
@@ -20,6 +17,15 @@ while read newLine; do
       a=("${a[@]:1}") # pop from the front
       samples=$((samples + 1))
     }
+    echo "set term $(echo $terminal) noraise"
+    echo "set yrange [0:$maxval]"
+    echo "set xrange [${samples}:$((samples+${#a[@]}-1))]"
+    echo "set style fill transparent solid 0.5"
+    echo -n "plot "
+    for ((j=0;j<$nf;++j)); do
+      echo -n " '-' u 1:$((j+2)) t '${titles[$j]}' w filledcurves x1 fc rgb '${colors[$j]}',"
+    done
+    echo
     for ((j=0;j<$nf;++j)); do
       tc=0 # temp counter
       for i in ${a[@]}; do
@@ -29,14 +35,5 @@ while read newLine; do
       echo e # gnuplot's end of dataset marker
     done
   }
-done | gnuplot -e "set yrange [0:$maxval]; \
-                   set term $(echo $terminal) noraise; \
-                   set xtics 1 rotate nomirror scale 0.0 font 'default,6'; \
-                   set style fill transparent solid 0.5; \
-                   plot '-' u 1:2 t '$legend1' w filledcurves x1 fc rgb 'red'\
-$([ -n "$4" ] && echo  ",'-' u 1:3 t '$4' w filledcurves x1 fc rgb 'blue'")\
-$([ -n "$5" ] && echo  ",'-' u 1:4 t '$5' w filledcurves x1 fc rgb 'green'")\
-$([ -n "$6" ] && echo  ",'-' u 1:5 t '$6' w filledcurves x1 fc rgb 'yellow'")\
-                   ;
-                   do for [i=0:100000000]{replot}" 2>/dev/null
+done | gnuplot 2>/dev/null
 
