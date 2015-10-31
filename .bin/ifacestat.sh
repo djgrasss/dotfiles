e8a0eef0bba39cc04815221c737c96b003b2ba46
Alex <flux242@gmail.com>
2015-10-31 06:19:48 +0100

    CHANGED: ifacestat handles counters overflow

--- .bin/ifacestat.sh
+++ .bin/ifacestat.sh
@@ -2,20 +2,22 @@
 
 ec='echo -e';[ -n "$($ec)" ] && ec='echo'
 
-f="/sys/class/net/${1:-wlan0}/statistics/"
+f="/sys/class/net/${1:-wlan0}/statistics"
+otx=$(cat "$f/tx_bytes")
+orx=$(cat "$f/rx_bytes")
+
 while [ -w /proc/self/fd/1 ]; do
   tx=$(cat "$f/tx_bytes")
   rx=$(cat "$f/rx_bytes")
-  c=$((tx+rx))
-  s="$(((c-${oc:-c})))"
-  [ $s -ge 0 ] && {
-    if [ -t 1 ]; then
-      ($ec -n "\033[s$s ${2+$((tx-${otx:-tx}))} ${2+$((rx-${orx:-rx}))}\033[K\033[u")
-    else
-      echo "$s ${2+$((tx-${otx:-tx}))} ${2+$((rx-${orx:-rx}))}"
-    fi
-  }
-  oc=$c
+  txd=$((tx-otx))
+  rxd=$((rx-orx))
+  [ $txd -lt 0 ] && txd=$((txd+4294967296))
+  [ $rxd -lt 0 ] && rxd=$((rxd+4294967296))
+  if [ -t 1 ]; then
+    ($ec -n "\033[s$((txd+rxd)) ${2+$txd} ${2+$rxd}\033[K\033[u")
+  else
+    echo "$((txd+rxd)) ${2+$txd} ${2+$rxd}"
+  fi
   otx=$tx
   orx=$rx
   sleep 1
