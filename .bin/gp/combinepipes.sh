09883e8ee8170f7a97434bdebeebc4e6644c651e
Alex <flux242@gmail.com>
2015-10-31 12:33:29 +0100

    CHANGED: synchronizes to the slowest pipe

--- .bin/gp/combinepipes.sh
+++ .bin/gp/combinepipes.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 # Usage example: 
-# ((cpustat.sh 0 | awk '{print "0:"$0;fflush()}') & \
-#  (cpustat.sh 1 | (while read line; do echo "1:$line";done)) & \
-#  (cpustat.sh 2 | (while read line; do echo "2:$line";done)) & \
-#  (cpustat.sh 3 | (while read line; do echo "3:$line";done))) | \
+# ((cpustat.sh 0 | bin/gp/markpipe.sh 0) & \
+#  (cpustat.sh 1 | bin/gp/markpipe.sh 1) & \
+#  (cpustat.sh 2 | bin/gp/markpipe.sh 2) & \
+#  (cpustat.sh 3 | bin/gp/markpipe.sh 3)) | \
 #  bin/combinepipes.sh 4
+
 # by default 2 pipes are combined
 pipes=${1:-2}
 
-awk -F: -v pipes=$pipes 'BEGIN {cnt=0}
-                         {
-                           a[$1]=$2; cnt++;
-                           if(cnt>pipes-1) {
+awk -F: -v pipes=$pipes '{
+                           a[$1]=$2;
+                           cnt=0;
+                           for (i in a) cnt++;
+                           if(cnt==pipes) {
                              i=0;
                              while(i<cnt) {
-                               printf("%s ", a[i++])
+                               printf("%s ", a[i])
+                               delete a[i++]
                              }
                              printf("\n")
                              fflush()
-                             cnt=0;
                            }
                          }' -
 
