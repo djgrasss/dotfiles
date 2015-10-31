51233da2c80559d7fb34ea6766f0f54e55d14691
Alex <flux242@gmail.com>
2015-10-31 12:57:51 +0100

    ADDED: min yrange is added and some cosmetic changes

--- .bin/gp/gnuplotwindow.sh
+++ .bin/gp/gnuplotwindow.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-winsize=${1:-60} # number of samples to show
-maxval=${2:-100} # maximum value of displayed y range. " " for infinity
-shift;shift      # the rest is the titles
+winsize=${1:-60}   # number of samples to show
+yrange=${2:-0:100} # min:max values of displayed y range.
+                   # ":" for +/- infinity. Default "0:100"
+shift;shift        # the rest are the titles
 
-terminal="wxt"   # terminal type (x11,wxt,qt)
+terminal="wxt"     # terminal type (x11,wxt,qt)
+style="w filledcurves x1"
+#style="w boxes"
+#style="w lines"
 
-samples=0        # samples counter
+samples=0          # samples counter
 IFS='\n'
 titles=( "$@" )
 colors=( "red" "blue" "green" "yellow" "cyan" "magenta")
@@ -19,12 +23,13 @@ while read newLine; do
       samples=$((samples + 1))
     }
     echo "set term $(echo $terminal) noraise"
-    echo "set yrange [0:$maxval]"
+    echo "set yrange [$yrange]"
     echo "set xrange [${samples}:$((samples+${#a[@]}-1))]"
     echo "set style fill transparent solid 0.5"
     echo -n "plot "
     for ((j=0;j<$nf;++j)); do
-      echo -n " '-' u 1:$((j+2)) t '${titles[$j]}' w filledcurves x1 "
+      echo -n " '-' u 1:$((j+2)) t '${titles[$j]}' "
+      echo -n "$style "
       [ -n "${colors[$j]}" ] && echo -n "fc rgb '${colors[$j]}'"
       echo -n ","
     done
