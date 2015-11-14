5bced8ea825d3b37abba1edfdf327ecb50896a77
Alex <flux242@gmail.com>
2015-11-14 21:55:39 +0100

    COSMETIC: toggle_border proper record terminator is used

--- .i3/toggle_border.sh
+++ .i3/toggle_border.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-border=$(i3-msg -t get_tree |awk -F"nodes" -v id=$(xprop -root|awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print strtonum($NF)}') '{for(i=1;i<=NF;++i){if (match($i,id)){print gensub(/.*border.:.([^"]*).*/,"\\1","g",$i)}}}')
+border=$(i3-msg -t get_tree |awk -F"id\":" -v id=$(xprop -root|awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print strtonum($NF)}') '{for(i=1;i<=NF;++i){if (match($i,id)){print gensub(/.*border.:.([^"]*).*/,"\\1","g",$i)}}}')
 if [ "$border" = "pixel" ]; then
   i3-msg border normal
 else
