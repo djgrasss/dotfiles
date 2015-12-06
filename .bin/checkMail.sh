359a2e6882c5d2334fb1f4085fdd577d2ee6706b
alex <alex@chrome>
2015-12-06 14:15:18 +0100

    CHANGED: checkMail proper inet connection check

--- .bin/checkMail.sh
+++ .bin/checkMail.sh
@@ -2,8 +2,16 @@
 
 WANIFACE=${1:-wlan0}
 
-/sbin/ifconfig $WANIFACE 2>/dev/null >/dev/null || exit 1
-mail=$(/usr/bin/fetchmail -c -f ~/fetchmail/.fetchmailrc 2>/dev/null) 
+#/sbin/ifconfig $WANIFACE 2>/dev/null >/dev/null || exit 1
+
+for iface in /sys/class/net/*; do
+  [ "$(basename $iface)" = "lo" ] && continue
+  [ "$(cat $iface/carrier)" = "1" ] && online=yes
+done
+echo "$online"
+[ -z "$online" ] && exit 1
+
+mail=$(/usr/bin/fetchmail -c -f ~/fetchmail/.fetchmailrc 2>/tmp/fetchmail.err) 
 ec=$?;[ $ec -gt 1 ] && exit $ec
 all=$(echo "$mail" | /bin/sed -nr 's/^([0-9]*).*/\1/p') 
 seen=$(echo "$mail" | /bin/sed -nr 's/[^\(]*.([^ ]*) seen.*/\1/p')
