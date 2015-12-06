aad465a96e96cf4970d794d9907912621030411c
Alex <flux242@gmail.com>
2015-12-06 12:15:55 +0100

    BUGFIX: wifi_connect works again with nm 0.9.x

--- .bin/wifi_connect.sh
+++ .bin/wifi_connect.sh
@@ -3,7 +3,7 @@
 IFS=$'\n'
 nmout=$(nmcli -m multiline -t -f ssid,bssid,freq,signal,security dev wifi)
 aps=$(echo "$nmout" | awk 'BEGIN{cnt=0}\
-                           {if(/^SSID/){ssid=gensub(/^SSID:\s*(.*)/,"\\1","g")};\
+                           {if(/^SSID/){ssid=gensub(/^SSID:(.*)/,"\\1","g")};\
                             if(/^BSSID/){bssid=gensub(/^BSSID:(.*)/,"\\1","g")};\
                             if(/^FREQ/){freq=gensub(/^FREQ:(.*)/,"\\1","g")};\
                             if(/^SIGNAL/){signal=gensub(/^SIGNAL:(.*)/,"\\1","g")};\
@@ -33,13 +33,17 @@ done
 # because ap name could contain that char so I use awk again
 ap_tuple=$(echo "$nmout"|awk -v ap="$ap" '{bssid=gensub(/^BSSID:(.*)/,"\\1","g");\
                                           if(flag==1){printf("%s\n%s\n",ssid,bssid);exit}\
-                                          else{ssid=gensub(/^SSID:\s*(.*)/,"\\1","g");if(ap ~ ssid){flag=1}}}')
+                                          else{ssid=gensub(/^SSID:(.*)/,"\\1","g");if(ap ~ ssid){flag=1}}}')
 ap_name=${ap_tuple%$'\n'*}
 ap_bssid=${ap_tuple#*$'\n'} # is not used any longer
-
 # make it compatible with the older nm versions
 cmdpar='list'
-nmcli con $cmdpar 2>/dev/null || cmdpar='show'
+nmcli con $cmdpar &>/dev/null || cmdpar='show'
+[ "$cmdpar" = "list" ] && {
+#  nm 0.9.x adds ' chars around ap_name 
+#  this is an ugly hack to remove them 
+  ap_name=$(awk -v ap="${ap_name}" 'BEGIN{print gensub(/.(.*?)./,"\\1","g",ap)}')
+}
 if nmcli con $cmdpar id "$ap_name" &>/dev/null; then
   nmcli con up id "$ap_name"
 else
