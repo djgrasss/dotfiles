0b0b5e0dca438a660bee4637a553d895cca6f6cd
alex <alex@chrome>
2016-01-24 22:40:00 +0100

    CHANGED: mail related scripts

--- .bin/catmail.sh
+++ .bin/catmail.sh
@@ -6,5 +6,20 @@
 #echo "To: $1"
 #echo "From: $2"
 
-awk '{if(f==1){print $0};if(match($0,/^Message-ID/)){f=1;for(i=0;i<55;++i){printf("-")};print ""}}' -
+cmd='cat -'
+[ -n "which lynx" ] && cmd='lynx -stdin'
+awk '
+{
+  if(f==1) {
+    print $0
+  };
+  IGNORECASE=1;
+  if(match($0,/^Message-ID/)) {
+    f=1;
+    for(i=0;i<55;++i) {
+      printf("-")
+    };
+    print "";
+  }
+}' - | $cmd
 
