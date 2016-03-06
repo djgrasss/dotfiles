3c05c4d0dd06cd31335af704ec03f84f8e74cb95
alex <alex@chrome>
2016-03-06 13:35:06 +0100

    ADDED: fft windowing

--- .bin/dsp/fft.sh
+++ .bin/dsp/fft.sh
@@ -1,10 +1,19 @@
 #/bin/bash
 
+# Usage example:
+# PLot the chirp signal and its frequency domain presentation:
+# (while true; do echo line; sleep 0.01; done)| \
+#  awk 'BEGIN{PI=atan2(0,-1);t=0;f0=1;f1=32;T=256;k=(f1-f0)/T;}{print sin(2*PI*(t*f0+k/2*t*t));t+=0.1;fflush()}'| \
+#  tee >(bin/dsp/fft.sh 64 "w1;0.5;0.5"|bin/dsp/fftabs.sh 64|bin/gp/gnuplotblock.sh '0:0.5' 'spectrum;1')| \
+#  bin/gp/gnuplotwindow.sh 128 "-1:1" "Sin wave;2"
 
-col=${1:-1}
-N=${2:-16}
 
-awk -v N=$N -v col=$col '
+N=${1:-64}   # number of samples, default 64
+wd=${2:-w0}  # window function description, w0-rectangular window (default), w1-hanning window
+col=${3:-1}  # input stream column to calculate fft for
+
+
+awk -v N=$N -v col=$col -v win_desc="$wd" '
 function compl_add(a, b,   ara,arb) {
   split(a, ara);split(b, arb);
   return ara[1]+arb[1]" "ara[2]+arb[2];
@@ -16,6 +25,39 @@ function compl_sub(a, b,   ara,arb) {
 function compl_mul(a, b,   ara,arb) {
   split(a, ara);split(b, arb);
   return ara[1]*arb[1]-ara[2]*arb[2]" "ara[1]*arb[2]+ara[2]*arb[1];
+}
+
+function init_hanning_window(N,a,b,     i)
+{
+  for(i=0;i<N;++i) {
+    w[i] = a - b*cos(2*PI*i/(N-1));
+  }
+}
+
+function init_window(N,win_desc,    i,wa,a,b)
+{
+  split(win_desc, wa, ";");
+  switch(wa[1]) {
+  case "w1": # hanning window
+    win_type = 1;
+    if (2 in wa) a = wa[2]; else a = 0.5;
+    if (3 in wa) b = wa[3]; else b = 0.5;
+    init_hanning_window(N,a,b);
+    break;
+  case "w0":
+  default:
+    win_type = 0;
+    break;
+  }
+}
+
+function apply_window(N,    i)
+{
+  if (win_type!=0) {
+    for(i=0;i<N;++i) {
+      c[i]*=w[i];
+    }
+  }
 }
 
 function calc_pow2(N,    pow)
@@ -37,9 +79,9 @@ function bit_reverse(n,pow,    i,r)
   return r;
 }
 
-function binary_inversion(N,    tmp,i,j,pow)
+function binary_inversion(N,pow,    tmp,i,j)
 {
-  pow = calc_pow2(N);
+#  pow = calc_pow2(N);
   for(i=0;i<N;++i) {
     j = bit_reverse(i,pow);
     if (i<j) {
@@ -87,13 +129,9 @@ BEGIN {
   PI = atan2(0,-1);
   expN = cos(2*PI/N) " " (-sin(2*PI/N));
   N2 = N/2;
-
-#  for(i=0;i<N2;++i) {c[i]=1}
-#  for(i=N2;i<N;++i) {c[i]=0}
-#  fft(0,N,expN); 
-#  binary_inversion(N);
-#  print_fft(N);
-#  exit(0); 
+  pow = calc_pow2(N);
+  win_type = 0; # rectangular window by default
+  init_window(N, win_desc);
 }
 {
   if (match($0,/^.+$/)) {
@@ -103,9 +141,12 @@ BEGIN {
       delete a[start];
       start++;
     } 
-    for (i=start;i<N+start;++i) {(i<end)?c[i-start] = a[i]:c[i-start] = 0;}
+    for (i=start;i<N+start;++i) {
+      (i<end)?c[i-start] = a[i]:c[i-start] = 0;
+    }
+    apply_window(N);
     fft(0,N,expN); 
-    binary_inversion(N);
+    binary_inversion(N,pow); # N = 2^pow;
     print_fft(N);
     printf("\n");
     fflush();
