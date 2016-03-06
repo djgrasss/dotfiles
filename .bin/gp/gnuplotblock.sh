3ea0d1c42f65bf9f30c127d91f0c7662d3c0a4cd
alex <alex@chrome>
2016-03-06 18:20:13 +0100

    ADDED: xy mode
    ADDED: x range input parameter parsing

--- .bin/gp/gnuplotblock.sh
+++ .bin/gp/gnuplotblock.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 terminal="qt"     # terminal type (x11,wxt,qt)
-yrange=${1:-0:100} # min:max values of displayed y range.
+range=${1:-0:100} # min:max values of displayed y range.
                    # ":" for +/- infinity. Default "0:100"
 shift              # the rest are the titles
 
-styles_def=( "filledcurves x1" "boxes" "lines" )
+declare -A styles_def
+styles_def=( [0]="filledcurves x1" [1]="boxes" [2]="lines" [3]="points" )
 # remove the color adjustment line below to get
 # default gnuplot colors for the first six plots
 colors_def=("red" "blue" "green" "yellow" "cyan" "magenta")
@@ -17,13 +18,19 @@ IFS=$';'
 while [ -n "$1" ]; do
   tmparr=( $1 )
   titles[$i]=${tmparr[0]}
-  styles[$i]=${styles_def[${tmparr[1]}]-${styles_def[0]}}
+  styles[$i]=${styles_def[${tmparr[1]}]-${tmparr[1]}}
+  [ -n "${styles[$i]}" ] || {styles=${styles_def[0]}}
   colors[$i]=${tmparr[2]-${colors_def[$i]}}
+  xy[$i]=${tmparr[3]}
   i=$((i+1))
   shift
 done
 
+tmparr=( $range )
+yrange=${tmparr[0]}
+xrange=${tmparr[1]}
 IFS=$'\n'
+
 samples=0          # samples counter
 (while read newLine; do
   if [ -n "$newLine" ]; then
@@ -35,14 +42,25 @@ samples=0          # samples counter
     IFS=$TMPIFS
     echo "set term $(echo $terminal) noraise"
     echo "set yrange [$yrange]"
-    echo "set xrange [-0.5:$((${#a[@]}-1)).5]"
+    if [ -n "$xrange" ]; then
+      echo "set xrange [$xrange]"
+    else
+      echo "set xrange [-0.5:$((${#a[@]}-1)).5]"
+    fi
     echo "set style fill transparent solid 0.5"
     echo -n "plot "
     for ((j=0;j<$nf;++j)); do
-      echo -n " '-' u 1:$((j+2)) t '${titles[$j]}' "
+      c1=1;
+      c2=$((j+2));
+      [ -n "${xy[j]}" ] && {
+        c1=$((j+2));
+        c2=$((j+3));
+      }
+      echo -n " '-' u $c1:$c2 t '${titles[$j]}' "
       echo -n "w ${styles[$j]-${styles_def[0]}} "
       [ -n "${colors[$j]}" ] && echo -n "fc rgb '${colors[$j]}'"
       echo -n ","
+      [ $c1 = 1 ] || j=$((j+1))
     done
     echo
     for ((j=0;j<$nf;++j)); do
