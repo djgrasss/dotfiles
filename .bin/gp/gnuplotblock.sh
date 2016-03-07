a8e851db56492e9fd245770783653a51d4ab0d7b
alex <alex@chrome>
2016-03-07 03:28:28 +0100

    ADDED: 3d plots

--- .bin/gp/gnuplotblock.sh
+++ .bin/gp/gnuplotblock.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-terminal="qt"     # terminal type (x11,wxt,qt)
-range=${1:-0:100} # min:max values of displayed y range.
+terminal="qt"      # terminal type (x11,wxt,qt)
+range=${1:-0:100}  # min:max values of displayed y range.
                    # ":" for +/- infinity. Default "0:100"
 shift              # the rest are the titles
+
+# titles definitions examples:
+# - "Spectrum;1;blue"
+# - "Scatter plot;points pointtype 5 pointsize 10;red;xy"
+# - "3D plot;2;#903489;3d;32;32"
 
 declare -A styles_def
 styles_def=( [0]="filledcurves x1" [1]="boxes" [2]="lines" [3]="points" )
@@ -21,18 +26,30 @@ while [ -n "$1" ]; do
   styles[$i]=${styles_def[${tmparr[1]}]-${tmparr[1]}}
   [ -n "${styles[$i]}" ] || {styles=${styles_def[0]}}
   colors[$i]=${tmparr[2]-${colors_def[$i]}}
-  xy[$i]=${tmparr[3]}
+  dtype[$i]=${tmparr[3]}
+  dtype_arg[$i]=${tmparr[4]}
+  dtype_arg2[$i]=${tmparr[5]}
   i=$((i+1))
   shift
 done
 
 tmparr=( $range )
-yrange=${tmparr[0]}
-xrange=${tmparr[1]}
+xrange=${tmparr[0]}
+yrange=${tmparr[1]}
+zrange=${tmparr[2]}
 IFS=$'\n'
-
 samples=0          # samples counter
-(while read newLine; do
+(
+ echo "set term $(echo $terminal) noraise"
+ echo "set style fill transparent solid 0.5"
+
+ echo "set xrange [$xrange]"
+ echo "set yrange [$yrange]"
+ echo "set zrange [$zrange]"
+# echo "set hidden3d"
+# echo "set dgrid3d 64,64"
+
+ while read newLine; do
   if [ -n "$newLine" ]; then
     a=("${a[@]}" "$newLine") # add to the end
   else
@@ -40,38 +57,49 @@ samples=0          # samples counter
     nf=0;TMPIFS=$IFS;IFS=$' 	\n'
       for j in ${a[0]};do nf=$((nf+1));done
     IFS=$TMPIFS
-    echo "set term $(echo $terminal) noraise"
-    echo "set yrange [$yrange]"
-    if [ -n "$xrange" ]; then
-      echo "set xrange [$xrange]"
-    else
-      echo "set xrange [-0.5:$((${#a[@]}-1)).5]"
-    fi
-    echo "set style fill transparent solid 0.5"
-    echo -n "plot "
-    for ((j=0;j<$nf;++j)); do
-      c1=1;
-      c2=$((j+2));
-      [ -n "${xy[j]}" ] && {
-        c1=$((j+2));
-        c2=$((j+3));
-      }
-      echo -n " '-' u $c1:$c2 t '${titles[$j]}' "
-      echo -n "w ${styles[$j]-${styles_def[0]}} "
-      [ -n "${colors[$j]}" ] && echo -n "fc rgb '${colors[$j]}'"
+    if [ "${dtype[0]}" = "3d" ]; then
+      echo -n "splot "
+      echo -n "'-' u 1:2:3 t '${titles[0]}' "
+      echo -n "w ${styles[0]-${styles_def[0]}} "
+      [ -n "${colors[0]}" ] && echo -n "fc rgb '${colors[0]}'"
       echo -n ","
-      [ $c1 = 1 ] || j=$((j+1))
-    done
+    else
+      echo -n "plot "
+      for ((j=0;j<$nf;++j)); do
+        c1=1; c2=$((j+2));
+        [ "${dtype[j]}" = "xy" ] && {
+          c1=$((j+2)); c2=$((j+3));
+        }
+        echo -n " '-' u $c1:$c2 t '${titles[$j]}' "
+        echo -n "w ${styles[$j]-${styles_def[0]}} "
+        [ -n "${colors[$j]}" ] && echo -n "fc rgb '${colors[$j]}'"
+        echo -n ","
+        [ $c1 = 1 ] || j=$((j+1))
+      done
+    fi
     echo
-    for ((j=0;j<$nf;++j)); do
-      tc=0 # temp counter
-      for i in ${a[@]}; do
-        echo "$tc $i"
-        tc=$((tc+1))
+    if [ "${dtype[0]}" = "3d" ]; then
+      [ ${#a[@]} -gt $((dtype_arg[0]*dtype_arg2[0])) ] && {
+        a=( "${a[@]:dtype_arg[0]}" )
+      }
+      for ((j=0;j<dtype_arg2[0];++j)); do
+        [ -n "${a[j*${dtype_arg[0]}]}" ] || break;
+        for ((i=0;i<dtype_arg[0];++i)); do
+          echo  "$i $j ${a[j*${dtype_arg[0]}+i]}"
+        done
       done
       echo e # gnuplot's end of dataset marker
-    done
-    unset a
+    else
+      for ((j=0;j<$nf;++j)); do
+        tc=0 # temp counter
+        for i in ${a[@]}; do
+          echo "$tc $i"
+          tc=$((tc+1))
+        done
+        echo e # gnuplot's end of dataset marker
+      done
+      unset a
+    fi
   fi 
 done) | gnuplot 2>/dev/null
 
