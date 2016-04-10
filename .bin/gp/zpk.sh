207ca2df27add15e0df369ab64e0724ab1f902ec
alex <alex@chrome>
2016-04-10 16:43:49 +0200

    CHANGED: zpk.sh performance improvements

--- .bin/gp/zpk.sh
+++ .bin/gp/zpk.sh
@@ -10,154 +10,154 @@ function zpk3D
 
   case "$ban" in
    1)
-    [ $m -gt 2 ] && zpk3D $((m/2))  6  $((xsign*-1))  $((ysign*-1))  $zsign;
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2))  6  $((-xsign))  $((-ysign))  $zsign;
+    echo "$X $Y $Z"
     Z=$((Z+zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 3  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 3  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     X=$((X+xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 3  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 3  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     Z=$((Z-zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 5  $((xsign*-1))  $ysign  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 5  $((-xsign))  $ysign  $((-zsign))
+    echo "$X $Y $Z"
     Y=$((Y+ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 5  $((xsign*-1))  $ysign  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 5  $((-xsign))  $ysign  $((-zsign))
+    echo "$X $Y $Z"
     Z=$((Z+zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 3  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 3  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     X=$((X-xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 3  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 3  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     Z=$((Z-zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 6  $((xsign*-1))  $ysign  $((zsign*-1))
-    [ $m -gt 2 ] || awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 6  $((-xsign))  $ysign  $((-zsign))
+    (( m > 2 )) || echo "$X $Y $Z"
     ;;
    2)
-    [ $m -gt 2 ] && zpk3D $((m/2)) 5  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 5  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     Y=$((Y+ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 4  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 4  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     X=$((X+xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 4  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 4  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     Y=$((Y-ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 6  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 6  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     Z=$((Z+zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 6  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 6  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     Y=$((Y+ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 4  $xsign  $((ysign*-1))  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 4  $xsign  $((-ysign))  $((-zsign))
+    echo "$X $Y $Z"
     X=$((X-xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 4  $xsign  $((ysign*-1))  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 4  $xsign  $((-ysign))  $((-zsign))
+    echo "$X $Y $Z"
     Y=$((Y-ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 5  $xsign  $((ysign*-1))  $((zsign*-1))
-    [ $m -gt 2 ] || awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 5  $xsign  $((-ysign))  $((-zsign))
+    (( m > 2 )) || echo "$X $Y $Z"
     ;;
    3)
-    [ $m -gt 2 ] && zpk3D $((m/2)) 1  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 1  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     Y=$((Y+ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 6  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 6  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     Z=$((Z+zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 6  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 6  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     Y=$((Y-ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 4  $((xsign*-1))  $ysign  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 4  $((-xsign))  $ysign  $((-zsign))
+    echo "$X $Y $Z"
     X=$((X+xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 4  $((xsign*-1))  $ysign  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 4  $((-xsign))  $ysign  $((-zsign))
+    echo "$X $Y $Z"
     Y=$((Y+ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 6  $xsign  $((ysign*-1))  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 6  $xsign  $((-ysign))  $((-zsign))
+    echo "$X $Y $Z"
     Z=$((Z-zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 6  $xsign  $((ysign*-1))  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 6  $xsign  $((-ysign))  $((-zsign))
+    echo "$X $Y $Z"
     Y=$((Y-ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 1  $((xsign*-1))  $((ysign*-1))  $zsign
-    [ $m -gt 2 ] || awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 1  $((-xsign))  $((-ysign))  $zsign
+    (( m > 2 )) || echo "$X $Y $Z"
     ;;
    4)
-    [ $m -gt 2 ] && zpk3D $((m/2)) 2  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 2  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     Z=$((Z+zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 5  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 5  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     Y=$((Y-ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 5  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 5  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     Z=$((Z-zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 3  $((xsign*-1))  $ysign  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 3  $((-xsign))  $ysign  $((-zsign))
+    echo "$X $Y $Z"
     X=$((X-xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 3  $((xsign*-1))  $ysign  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 3  $((-xsign))  $ysign  $((-zsign))
+    echo "$X $Y $Z"
     Z=$((Z+zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 5  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 5  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     Y=$((Y+ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 5  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 5  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     Z=$((Z-zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 2  $xsign  $((ysign*-1))  $((zsign*-1))
-    [ $m -gt 2 ] || awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 2  $xsign  $((-ysign))  $((-zsign))
+    (( m > 2 )) || echo "$X $Y $Z"
     ;;
    5)
-    [ $m -gt 2 ] && zpk3D $((m/2)) 4  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 4  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     X=$((X+xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 2  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 2  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     Z=$((Z+zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 2  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 2  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     X=$((X-xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 1  $((xsign*-1))  $ysign  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 1  $((-xsign))  $ysign  $((-zsign))
+    echo "$X $Y $Z"
     Y=$((Y+ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 1  $((xsign*-1))  $ysign  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 1  $((-xsign))  $ysign  $((-zsign))
+    echo "$X $Y $Z"
     X=$((X+xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 2  $xsign  $((ysign*-1))  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 2  $xsign  $((-ysign))  $((-zsign))
+    echo "$X $Y $Z"
     Z=$((Z-zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 2  $xsign  $((ysign*-1))  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 2  $xsign  $((-ysign))  $((-zsign))
+    echo "$X $Y $Z"
     X=$((X-xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 4  $xsign  $ysign  $zsign
-    [ $m -gt 2 ] || awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 4  $xsign  $ysign  $zsign
+    (( m > 2 )) || echo "$X $Y $Z"
     ;;
    6)
-    [ $m -gt 2 ] && zpk3D $((m/2)) 3  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 3  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     X=$((X-xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 1  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 1  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     Y=$((Y-ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 1  $((xsign*-1))  $((ysign*-1))  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 1  $((-xsign))  $((-ysign))  $zsign
+    echo "$X $Y $Z"
     X=$((X+xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 2  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 2  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     Z=$((Z+zsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 2  $xsign  $ysign  $zsign
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 2  $xsign  $ysign  $zsign
+    echo "$X $Y $Z"
     X=$((X-xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 1  $((xsign*-1))  $ysign  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 1  $((-xsign))  $ysign  $((-zsign))
+    echo "$X $Y $Z"
     Y=$((Y+ysign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 1  $((xsign*-1))  $ysign  $((zsign*-1))
-    awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 1  $((-xsign))  $ysign  $((-zsign))
+    echo "$X $Y $Z"
     X=$((X+xsign));
-    [ $m -gt 2 ] && zpk3D $((m/2)) 3  $xsign  $((ysign*-1))  $((zsign*-1))
-    [ $m -gt 2 ] || awk -v x=$X -v y=$Y -v z=$Z -v n=$((N-1)) 'BEGIN{print x/n" "y/n" "z/n}'
+    (( m > 2 )) && zpk3D $((m/2)) 3  $xsign  $((-ysign))  $((-zsign))
+    (( m > 2 )) || echo "$X $Y $Z"
     ;;
    *) echo "Error: zpk rotation index is wrong: $m" >&2;;
   esac 
@@ -168,15 +168,15 @@ iteration=${1:-1}
 while true; do
   N=$((1<<iteration))
   X=0; Y=0; Z=0;
-  zpk3D $N 1  1 1 1
+  zpk3D $N 1  1 1 1 | awk -v N=$((N-1)) '{print $1/N" "$2/N" "$3/N;fflush()}'
   echo
-
+  [ -n "$2" ] && exit 0
   while true; do
     read -s -n1 c
     case $c in
       s) [ $iteration -gt 1 ] && iteration=$((iteration-1));break ;;
       w) [ $iteration -lt 8 ] && iteration=$((iteration+1));break ;;
-      *) echo >&2
+      *) echo "$iteration" >&2
     esac
   done
 done
