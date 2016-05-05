de751608607d842c2bd6d9b18ca295916925879d
alex <alex@chrome>
2016-05-05 13:16:10 +0200

    CHANGED: randomwallpaper script simplified

--- .bin/randomwallpaper.sh
+++ .bin/randomwallpaper.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 wpath=${1:-~/wallpapers}
-img_count=$(ls -1 ${wpath}/* | wc -l)
-
-feh --bg-scale $(sed -n $(( (RANDOM % img_count)+1))p <(ls -1 ${wpath}/*))
+feh --bg-scale $(ls -1 ${wpath}/*|shuf|head -n1)
 
