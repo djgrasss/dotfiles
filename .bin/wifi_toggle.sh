00a9a195eb217adde2214bca4dc0dc4d2077b882
alex <alex@chrome>
2016-11-28 10:26:54 +0100

    BUGFIX: wifi_toggle is updated to use new nmcli interface

--- .bin/wifi_toggle.sh
+++ .bin/wifi_toggle.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-wifistat=$(nmcli nm wifi)
+wifistat=$(nmcli radio wifi)
 if [ "disabled" = ${wifistat#*\ } ]; then
-  nmcli nm wifi on
-  wifistat=$(nmcli nm wifi)
+  nmcli radio wifi on
+  wifistat=$(nmcli radio wifi)
   notify-send -u low -i /usr/share/icons/elementary-xfce/status/48/wifi-100.png "WiFi status:" "WiFi is now ${wifistat#*\ }"
 else
-  nmcli nm wifi off
-  wifistat=$(nmcli nm wifi)
+  nmcli radio wifi off
+  wifistat=$(nmcli radio wifi)
   notify-send -u low -i /usr/share/icons/elementary-xfce/status/48/wifi-100.png "WiFi status:" "WiFi is now ${wifistat#*\ }"
 fi
 
