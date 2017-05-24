0e8d95fb365f1574b31a0fd6d1ac7dfa3bfc09e0
alex <alex@chrome>
2017-05-24 14:14:15 +0200

    COSMETIC: isonbattery.sh cleaned

--- .bin/isonbattery.sh
+++ .bin/isonbattery.sh
@@ -4,9 +4,7 @@
 # with 1 otherwise. 
 
 for i in /sys/class/power_supply/*; do
-  echo "$i" >> /tmp/isonbattery.log
   [ -e "$i/online" ] && [ "1" = "$(cat $i/online)" ] && exit 1
 done
-  echo "exist with 0" >> /tmp/isonbattery.log
 exit 0
 
