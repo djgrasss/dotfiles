5069bf8e00916a3acb9b3c47148cb4bbd54c88b7
alex <alex@chrome>
2017-12-16 13:13:42 +0100

    CHANGED: vim_project uses pwd if no folder specified

--- .bin/vim_project.sh
+++ .bin/vim_project.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-[[ -z "$1" ]] || [[ ! -d "$1" ]] && {
-  echo "Usage: $0 project_dir" >&2
-  exit 1
+PROJDIR=$1
+[[ -z "$PROJDIR" ]] || [[ ! -d "$PROJDIR" ]] && {
+  PROJDIR=$(pwd)
+  echo "Project dir is not specified, using current directory - $PROJDIR" >&2
+#  exit 1
 }
 
-PROJDIR=$(realpath "$1")
+PROJDIR=$(realpath "$PROJDIR")
 export PROJDIR
 cd "$PROJDIR" || exit 1
 #ctags -aR ./
